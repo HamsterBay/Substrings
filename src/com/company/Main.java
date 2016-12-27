6aea66ff4b52aecf90c54964d8e8142555160c48
Kasia Vasiluk <kasia.vasiluk@gmail.com>
2016-12-27 23:55:41 +0100

    Add saving to a file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
             App app = new App();
-            app.start();
-
+            app.substring();
         }
     }
-}
